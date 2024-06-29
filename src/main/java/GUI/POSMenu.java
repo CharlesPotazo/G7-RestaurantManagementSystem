84ba304d1fe712fcea0d066aaf65cbd0342e233f
Daniel Jose V. Sagun <danieljosesagun@gmail.com>
2024-06-29 11:00:01 +0800

    add food panel functionalities in POSMenu

--- src/main/java/GUI/POSMenu.java
+++ src/main/java/GUI/POSMenu.java
@@ -31,7 +31,7 @@ public class POSMenu extends JFrame implements ActionListener{
     
     private static final String URL = "jdbc:mysql://localhost:3306/reports"; //your_localhost
     private static final String USER = "root";
-    private static final String PASSWORD = "password"; //your_password
+    private static final String PASSWORD = "your_password"; //password in Mysql
     
     
     POSMenu(){
